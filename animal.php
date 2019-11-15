a0a50863d2b140d01e856d4be2f48c1dbceef90b
RICARDO <thequeen20156@gmail.com>
2019-11-14 19:20:33 -0500

    creacion de la clase animal

---
+++ animal.php
@@ -0,0 +1,15 @@
+<?php
+class animal{
+    private $id;
+    private $nombre;
+    private $peso;
+
+    public function getid(){
+        return $this->id;
+    }
+    public function getnombre(){
+        return $this->id;
+    }
+    public function getpeso(){
+        return $this->peso;
+    }
